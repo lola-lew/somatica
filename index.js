00355ff1848bd8955188d6ce7157e85b04547291
unknown <lolarlew@gmail.com>
2022-09-09 21:04:24 -0300

    added bootstrap

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.get('/home', (req, res) => {
-  res.send('This is the homepage')
+  res.render('home')
 })
 
 app.listen(3000, () => {
